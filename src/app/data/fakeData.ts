89c867ac44d02c5dd325c77bf6aadcc08b3c8899
Julio Borges <juliocesarsantosborges@gmail.com>
2023-10-22 08:59:17 -0300

    Adicionado recuperação dinâmica de informações para as páginas

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,10 @@
+export const dataFake =
+    [
+        {
+            "id": "1",
+            "photo": 'https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png',
+            "title": "Lorem ipsum",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec metus lectus, volutpat a blandit ac, mollis dictum nisl. Suspendisse erat ligula, eleifend nec nunc id, luctus venenatis quam.Mauris interdum consequat dolor, eu dictum nibh lobortis at.Mauris mattis justo nec velit luctus, eu ullamcorper diam tristique.Vivamus rutrum nec augue ac sodales."
+
+        }
+    ]
